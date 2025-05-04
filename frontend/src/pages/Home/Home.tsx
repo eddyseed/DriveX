50bd320e199e4dc314762368f48964979ee8341c
eddyseed <randomboiii069@gmail.com>
2025-05-04 16:47:39 +0530

    fix: rename state variable for login success and add handling for car addition info

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -8,11 +8,12 @@ import { useEffect, useState } from 'react';
 
 const Home: React.FC = () => {
   const location = useLocation();
-  const [showSuccess, setShowSuccess] = useState(false);
+  const [showLoginSuccess, setShowLoginSuccess] = useState(false);
   const [logoutSuccess, setLogoutSuccess] = useState(false);
+  const [carAdditionInfo, setCarAdditionInfo] = useState(false);
   useEffect(() => {
     if (location.state?.loginSuccess) {
-      setShowSuccess(true);
+      setShowLoginSuccess(true);
       window.history.replaceState({}, document.title);
     }
     const urlParams = new URLSearchParams(window.location.search);
@@ -22,16 +23,26 @@ const Home: React.FC = () => {
       window.history.replaceState({}, document.title);
       window.history.pushState({}, document.title, '/');
     }
+    // const carAdded = urlParams.get('carAdded');
+    // if (carAdded) {
+    //   setCarAdditionInfo(true);
+    //   window.history.replaceState({}, document.title);
+    //   window.history.pushState({}, document.title, '/');
+    // }
+    if(location.state?.carAdded) {
+      setCarAdditionInfo(true);
+      window.history.replaceState({}, document.title);
+    }
   }, [location]);
 
   return (
     <div>
-      {showSuccess && (
+      {showLoginSuccess && (
         <SuccessModal
           successHead="Welcome Back"
           successMsg="Login Successful"
           visible={true}
-          onClose={() => setShowSuccess(false)}
+          onClose={() => setShowLoginSuccess(false)}
         />
       )}
       {logoutSuccess && (
@@ -40,6 +51,14 @@ const Home: React.FC = () => {
           successMsg="You have been logged out successfully."
           visible={true}
           onClose={() => setLogoutSuccess(false)}
+        />
+      )}
+      {carAdditionInfo && (
+        <SuccessModal
+          successHead="Car Added"
+          successMsg="Your car has been added successfully."
+          visible={true}
+          onClose={() => setCarAdditionInfo(false)}
         />
       )}
       <LandingPage />
