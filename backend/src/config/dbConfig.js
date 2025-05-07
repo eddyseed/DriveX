76bbd6124f7c0c77b24abb8c878ec1f816ca544e
eddyseed <randomboiii069@gmail.com>
2025-05-07 16:32:22 +0530

    fix: update Supabase client initialization to use ANON_KEY instead of DATABASE_KEY

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -17,5 +17,5 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config()
 
-const supabase = createClient(process.env.DATABASE_URL, process.env.DATABASE_KEY);
+const supabase = createClient(process.env.DATABASE_URL, process.env.ANON_KEY);
 module.exports = supabase;
